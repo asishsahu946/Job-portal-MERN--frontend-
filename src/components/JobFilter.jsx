8e8beb76e340f9000d736f901cfac0f0767612b6
asishsahu946 <asishsahu946@gmail.com>
2025-02-23 19:47:55 +0530

    bug fixed

--- src/components/JobFilter.jsx
+++ src/components/JobFilter.jsx
@@ -20,7 +20,7 @@ function JobFilter() {
    useEffect(() => {
       const fetchJobs = async () => {
         try {
-          const response = await fetch("https://jobportalmernbackend.vercel.app/filterjobs");
+          const response = await fetch("https://jobportalmernbackend.vercel.app/getjobs");
           if (!response.ok) {
             throw new Error("Failed to fetch jobs");
           }
